0e7cecb51bd48feb99d74fe9f60a6cab28c2ff81
homdev <44805789+homdev@users.noreply.github.com>
2023-02-26 11:48:02 +0100

    init

--- pages/api/scrap.js
+++ pages/api/scrap.js
@@ -1,14 +1,42 @@
-// pages/api/scrap.js
+// Importe les fonctions nécessaires du module de base de données
+const { getCarsForSite, saveCarToDatabase, removeDuplicateCars } = require('../../lib/database');
 
+// Importe les modules de scraping pour les sites autoscout24 et lacentrale
+const { autoScout24 } = require('../../lib/autoscout24');
+const { laCentrale } = require('../../lib/lacentrale');
 
-import { getAllCars } from '../../lib/database';
-
+// Définit la fonction de gestionnaire qui sera appelée par la route
 export default async function handler(req, res) {
   try {
-    const cars = await getAllCars();
-    res.status(200).json(cars);
+    const site = req.query.site;
+
+    let cars;
+    if (site === 'autoScout24') {
+      // Scrape les voitures du site 1
+      // cars = await autoScout24(20); // 10 est la valeur de maxPage
+    } else if (site === 'laCentrale') {
+      // Scrape les voitures du site 2
+      // cars = await laCentrale(500); // 10 est la valeur de maxPage
+    }
+
+    if (cars) { // vérifie si cars est défini
+      // Sauvegarde les voitures dans la base de données en indiquant le nom du site
+      await Promise.all(cars.map(car => saveCarToDatabase(car, site)));
+
+      // Supprime les voitures en double de la base de données pour le site
+      await removeDuplicateCars(site);
+    } else {
+      console.error(`Erreur de scraping des voitures : ${site} a retourné undefined`);
+    }
+
+    // Récupère toutes les voitures pour le site depuis la base de données
+    const carsForSite = await getCarsForSite(site);
+
+    // Retourne les voitures pour le site au format JSON avec un code d'état de 200
+    res.status(200).json(carsForSite);
   } catch (error) {
-    console.error('Error retrieving cars:', error);
-    res.status(500).json({ error: 'An error occurred while retrieving cars.' });
+    console.error('Erreur de scraping des voitures :', error);
+    // Retourne un message d'erreur au format JSON avec un code d'état de 500 en cas d'erreur
+    res.status(500).json({ error: 'Une erreur s\'est produite lors du scraping des voitures.' });
   }
 }
--- pages/index-page.js
+++ pages/index-page.js
@@ -1,17 +1,18 @@
 import { useState, useEffect, useCallback } from 'react';
 import styles from '../styles/ScrapPage.module.css';
 
-
 function ScrapComponent() {
   const [cars, setCars] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [carCount, setCarCount] = useState(0);
 
   const fetchCars = useCallback(async () => {
     setIsLoading(true);
-    const response = await fetch('/api/scrap');
-    const data = await response.json();
-    setCars(data);
+    const response1 = await fetch('/api/scrap?site=autoScout24');
+    const response2 = await fetch('/api/scrap?site=laCentrale');
+    const data1 = await response1.json();
+    const data2 = await response2.json();
+    const carsWithSite = data1.map(car => ({ ...car, site: 'autoScout24' })).concat(data2.map(car => ({ ...car, site: 'laCentrale' })));
+    setCars(carsWithSite);
     setIsLoading(false);
   }, []);
 
@@ -19,49 +20,44 @@ function ScrapComponent() {
     fetchCars();
   }, [fetchCars]);
 
-  useEffect(() => {
-    async function fetchCarCount() {
-      const response = await fetch('/api/scrap');
-      const data = await response.json();
-      setCarCount(data.count);
-    }
-
-    fetchCarCount();
-  }, []);
-
   return (
-    <div className={styles['table-container']}>
-      {isLoading && <div className={styles.loader}>Loading...</div>}
-      <table>
-        <thead>
-          <tr>
-            <th>Marque</th>
-            <th>Modèle</th>
-            <th>Prix</th>
-            <th>Kilométrage</th>
-            <th>Années</th>
-            <th>Puissance</th>
-            <th>Carburant</th>
-            <th>Nb Propriétaire</th>
-            <th>Transmission</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cars.map((car, index) => (
-            <tr key={index}>
-              <td>{car.make}</td>
-              <td>{car.model}</td>
-              <td>{car.price}</td>
-              <td>{car.mileage}</td>
-              <td>{car.year}</td>
-              <td>{car.horsepower}</td>
-              <td>{car.fuelType}</td>
-              <td>{car.ownerCount}</td>
-              <td>{car.transmission}</td>
+    <div>
+      <div className={styles['table-container']}>
+        <h2>Toutes les voitures</h2>
+        {isLoading && <div className={styles.loader}>Loading...</div>}
+        <table>
+          <thead>
+            <tr>
+              <th>Site</th>
+              <th>Marque</th>
+              <th>Modèle</th>
+              <th>Prix</th>
+              <th>Kilométrage</th>
+              <th>Années</th>
+              <th>Puissance</th>
+              <th>Carburant</th>
+              <th>Nb Propriétaire</th>
+              <th>Transmission</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {cars.map((car, index) => (
+              <tr key={index}>
+                <td>{car.site}</td>
+                <td>{car.make}</td>
+                <td>{car.model}</td>
+                <td>{car.price}</td>
+                <td>{car.mileage}</td>
+                <td>{car.year}</td>
+                <td>{car.horsepower}</td>
+                <td>{car.fuelType}</td>
+                <td>{car.ownerCount}</td>
+                <td>{car.transmission}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
